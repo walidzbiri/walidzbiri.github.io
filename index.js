101c990f9076efb4d2534d04fd394c86f598c7e6
Walid Zbiri <43625951+walidzbiri@users.noreply.github.com>
2022-03-22 14:07:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+var i = 0;
+var txt =
+  "Full-stack software developer and architect, in love with everything related to programming, leadership and innovation. I am also a doctor.";
+var speed = 30;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  typeWriter();
+});
+
+function openTab(evt, tabName) {
+  console.log(tabName);
+  tabs = document.getElementsByClassName("tab");
+
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
+var form = document.getElementById("contact-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("contact-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
